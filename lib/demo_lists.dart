5fcedc88d702b0f63d97e679a37812e517c56a63
zhang xiuming <zhangxiuming@zepp.com>
2022-12-13 19:57:09 +0800

    初始化demo程序展示列表

---
+++ lib/demo_lists.dart
@@ -0,0 +1,37 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class DemoConfig {
+  final String title;
+  final Widget page;
+
+  DemoConfig({required this.title, required this.page});
+}
+
+final List<DemoConfig> allDemos = [
+  DemoConfig(title: "Demo31", page: Text("Demo1")),
+  DemoConfig(title: "Demo3", page: Text("Demo1")),
+  DemoConfig(title: "Demo1", page: Text("Demo1")),
+  DemoConfig(title: "Demo1", page: Text("Demo1")),
+  DemoConfig(title: "Demo1", page: Text("Demo1")),
+  DemoConfig(title: "Demo1", page: Text("Demo1")),
+  DemoConfig(title: "Demo1", page: Text("Demo1")),
+  DemoConfig(title: "Demo1", page: Text("Demo1")),
+  DemoConfig(title: "Demo1", page: Text("Demo1")),
+  DemoConfig(title: "Demo1", page: Text("Demo1")),
+  DemoConfig(title: "Demo33", page: Text("Demo1")),
+  DemoConfig(title: "Demo2", page: Text("Demo1")),
+  DemoConfig(title: "Demo31", page: Text("Demo1")),
+  DemoConfig(title: "Demo3", page: Text("Demo1")),
+  DemoConfig(title: "Demo1", page: Text("Demo1")),
+  DemoConfig(title: "Demo1", page: Text("Demo1")),
+  DemoConfig(title: "Demo1", page: Text("Demo1")),
+  DemoConfig(title: "Demo1", page: Text("Demo1")),
+  DemoConfig(title: "Demo1", page: Text("Demo1")),
+  DemoConfig(title: "Demo1", page: Text("Demo1")),
+  DemoConfig(title: "Demo1", page: Text("Demo1")),
+  DemoConfig(title: "Demo1", page: Text("Demo1")),
+  DemoConfig(title: "Demo33", page: Text("Demo1")),
+  DemoConfig(title: "Demo22333", page: Text("Demo1")),
+];
